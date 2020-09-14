765624ff14239f534507e5f0572d553bbe60d9d7
Amir Sejdinovic <amir24V@yahoo.com>
2020-09-14 13:58:34 +0200

    Outputing the data from array via ngFor directive

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//Importing the custom ts class for ingerdinets
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  //Defined the typ of ingredients model which I created in folder shared
+  ingredients: Ingredient[] = [
+    //Creating the ingredient vis ts class
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatos',3)
+
+  ];
 
   constructor() { }
 
