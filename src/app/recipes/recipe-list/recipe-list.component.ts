d4ed461efdb629b99503cc91526bdbb81e4ab743
Amir Sejdinovic <amir24V@yahoo.com>
2020-09-14 10:54:20 +0200

    Creating the recipe model (recipe class )
    This model (TS class) will be the blueprint for all the recipes.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  //This will be array of recipes, and it is now empty. It will be filed with array methods
+  recipes = [];
 
   constructor() { }
 
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,16 @@
+
+//Exportzing the ts class
+//Here I create the vanila TS class and exprot it. This class will be a blueprint for the all recipes
+export class Recipe{
+     //Here I create the propreties of the class and asigned them the public keyword because it can called otuside of the class. I also give the properties the specific type that it will recive
+     public name: string;
+     public description: string;
+     public imagePath: string;
+      //Here i create the constructor method which will allow me to call the class with the new keyword. The constructor method will ran always on creating new instance of the class
+     constructor(name:string, desc:string, imagePath:string){
+         //Assigning the arguments that we recive to the propreties of the class
+         this.name = name;
+         this.description = desc;
+         this.imagePath = imagePath;
+     }
+}
