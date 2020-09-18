2c8ff510a9ac75f8f271ed42a59ded871f907a03
Amir Sejdinovic <amir24V@yahoo.com>
2020-09-18 14:11:26 +0200

    Creating the detal view of recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {Recipe} from '../../recipe.model';
 
 @Component({
@@ -8,10 +8,17 @@ import {Recipe} from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  //Creating the new proprety with emiiter and this emitter will not held any info because of that I set this type to void
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //Creating the onSelected method which will be handling the click on the a element
+  //This method will emti the event
+  onSelected(){
+    this.recipeSelected.emit();
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 //Importing the Recipe class
 import { Recipe } from '../recipe.model';
 
@@ -8,16 +8,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  //Emitter
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   //This will be array of recipes, and it is now empty. It will be filed with array methods
   //I here defined that proprety will be use the Recipe model 
   recipes: Recipe[] = [
+    
     //Creating new Recipe based on our blueprint eg Recipe class
-    new Recipe('A test Recipe','This is a test','https://p0.pikist.com/photos/202/820/gastronomy-food-dishes-eat-recipe-lunch.jpg')
+    new Recipe('A test Recipe','This is a test','https://p0.pikist.com/photos/202/820/gastronomy-food-dishes-eat-recipe-lunch.jpg'),
+    new Recipe('Novi recept','Opis recepta','https://townsquare.media/site/959/files/2020/06/GettyImages-614515684.jpg?w=980&q=75')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //Creating method which will emit the data to another component
+  onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
