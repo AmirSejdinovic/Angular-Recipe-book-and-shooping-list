dde4f1b415a919eece7b25286030be3813b7ab76
Amir Sejdinovic <amir24V@yahoo.com>
2020-09-18 14:44:36 +0200

    Enable users tu input the ingredients into the app

--- src/app/shopping-list/edit/edit.component.ts
+++ src/app/shopping-list/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  //Creating proprety which will recive data from input field via localReferenc
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amounInputRef: ElementRef;
+  //Emitt
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //Creating custom method
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmo = this.amounInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName,ingAmo);
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
