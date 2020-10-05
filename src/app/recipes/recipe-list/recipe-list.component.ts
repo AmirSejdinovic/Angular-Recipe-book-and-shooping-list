d0964c46c99aab7360e1ee466f737321b7462adf
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2020-10-05 13:47:18 +0200

    Moving the array of recipes in the recipes service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,32 +1,28 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 //Importing the Recipe class
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   //Emitter
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   //This will be array of recipes, and it is now empty. It will be filed with array methods
-  //I here defined that proprety will be use the Recipe model 
-  recipes: Recipe[] = [
-    
-    //Creating new Recipe based on our blueprint eg Recipe class
-    new Recipe('A test Recipe','This is a test','https://p0.pikist.com/photos/202/820/gastronomy-food-dishes-eat-recipe-lunch.jpg'),
-    new Recipe('Novi recept','Opis recepta','https://townsquare.media/site/959/files/2020/06/GettyImages-614515684.jpg?w=980&q=75')
-  ];
-
-  constructor() { }
+  //I here defined that proprety will be use the Recipe model
+  recipes: Recipe[] = [];
+  //This is typescript shortcut to insant asigne the proprety with same name
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   //Creating method which will emit the data to another component
-  onRecipeSelected(recipe: Recipe){
-      this.recipeWasSelected.emit(recipe);
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,22 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+//Creating the typescript class whicl will be our service
+export class RecipeService {
+  private recipes: Recipe[] = [
+    //Creating new Recipe based on our blueprint eg Recipe class
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://p0.pikist.com/photos/202/820/gastronomy-food-dishes-eat-recipe-lunch.jpg'
+    ),
+    new Recipe(
+      'Novi recept',
+      'Opis recepta',
+      'https://townsquare.media/site/959/files/2020/06/GettyImages-614515684.jpg?w=980&q=75'
+    ),
+  ];
+  //Creating the metod
+  getRecipes() {
+    //Return the array above. This will return the new array. Slice method will return the copy of array above and nobody can directly access to this above array
+    return this.recipes.slice();
+  }
+}
