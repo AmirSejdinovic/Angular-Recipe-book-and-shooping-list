771d51906fe84599828cd74dd6be2ab9c06598f8
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2020-10-06 11:19:48 +0200

    Adding ingredients in the discription

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,16 +1,25 @@
-
 //Exportzing the ts class
+
+import { Ingredient } from '../shared/ingredient.model';
+
 //Here I create the vanila TS class and exprot it. This class will be a blueprint for the all recipes
-export class Recipe{
-     //Here I create the propreties of the class and asigned them the public keyword because it can called otuside of the class. I also give the properties the specific type that it will recive
-     public name: string;
-     public description: string;
-     public imagePath: string;
-      //Here i create the constructor method which will allow me to call the class with the new keyword. The constructor method will ran always on creating new instance of the class
-     constructor(name:string, desc:string, imagePath:string){
-         //Assigning the arguments that we recive to the propreties of the class
-         this.name = name;
-         this.description = desc;
-         this.imagePath = imagePath;
-     }
+export class Recipe {
+  //Here I create the propreties of the class and asigned them the public keyword because it can called otuside of the class. I also give the properties the specific type that it will recive
+  public name: string;
+  public description: string;
+  public imagePath: string;
+  public ingredients: Ingredient[];
+  //Here i create the constructor method which will allow me to call the class with the new keyword. The constructor method will ran always on creating new instance of the class
+  constructor(
+    name: string,
+    desc: string,
+    imagePath: string,
+    ingredients: Ingredient[]
+  ) {
+    //Assigning the arguments that we recive to the propreties of the class
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 //Creating the typescript class whicl will be our service
 export class RecipeService {
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'A test Recipe',
       'This is a test',
-      'https://p0.pikist.com/photos/202/820/gastronomy-food-dishes-eat-recipe-lunch.jpg'
+      'https://p0.pikist.com/photos/202/820/gastronomy-food-dishes-eat-recipe-lunch.jpg',
+      [new Ingredient('Meet', 1), new Ingredient('Fries', 21)]
     ),
     new Recipe(
       'Novi recept',
       'Opis recepta',
-      'https://townsquare.media/site/959/files/2020/06/GettyImages-614515684.jpg?w=980&q=75'
+      'https://townsquare.media/site/959/files/2020/06/GettyImages-614515684.jpg?w=980&q=75',
+      [new Ingredient('Bunst', 1), new Ingredient('Meet', 21)]
     ),
   ];
   //Creating the metod
