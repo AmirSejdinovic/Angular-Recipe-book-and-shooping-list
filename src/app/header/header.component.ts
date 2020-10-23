041df639bd2e189b7264e179fc11003d0e5eacb4
Amir Sejdinovic <amir24V@yahoo.com>
2020-10-23 10:44:59 +0200

    Creating links via routerLink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +8,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 export class HeaderComponent {
 
-   //Creating the proprety which I will emit via EventEmitter method
-   //Inside the EventEmitter i defined that it will emit the string type
-   @Output() featureSelected = new EventEmitter<string>();
 
-    //Creating the onSelect method which will held all the logic
-    onSelect(feature: string){
-      //Passing the custom propreti tu emiter
-      this.featureSelected.emit(feature);
-
-
-    }
+    
 }
