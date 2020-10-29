49791c3dcfff34259f1776de91190bb379b4169a
Amir Sejdinovic <amir24V@yahoo.com>
2020-10-29 11:53:20 +0100

    Ading links and methods

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 //Importing the Recipe class
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -11,9 +12,12 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
   //This is typescript shortcut to insant asigne the proprety with same name
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
