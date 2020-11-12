a1b151028d7a0c12d0632ddca20bad1112f64369
Amir Sejdinovic <amir24V@yahoo.com>
2020-11-12 14:25:53 +0100

    Restucturing

--- src/app/shopping-list/edit/edit.component.ts
+++ src/app/shopping-list/edit/edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,17 +9,31 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
   //Creating proprety which will recive data from input field via localReferenc
 
 
   constructor(private slService: ShoppingListService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.slService.starterEditing.subscribe(
+      (index: number)=>{
+        this.editedItemIndex = index;
+         this.editMode = true;
+      }
+    );
+  }
   //Creating custom method
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
   ingedientsChanges = new EventEmitter<Ingredient[]>();
   //Defined the typ of ingredients model which I created in folder shared
+  starterEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     //Creating the ingredient vis ts class
     new Ingredient('Apples', 5),
