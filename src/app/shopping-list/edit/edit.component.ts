644512ec5f8a0196467175c4ee02d9ebe5e71404
Amir Sejdinovic <amir24V@yahoo.com>
2020-11-12 13:29:57 +0100

    Adding the angular form template aproach

--- src/app/shopping-list/edit/edit.component.ts
+++ src/app/shopping-list/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -9,17 +10,15 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class EditComponent implements OnInit {
   //Creating proprety which will recive data from input field via localReferenc
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amounInputRef: ElementRef;
+
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {}
   //Creating custom method
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmo = this.amounInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmo);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 }
