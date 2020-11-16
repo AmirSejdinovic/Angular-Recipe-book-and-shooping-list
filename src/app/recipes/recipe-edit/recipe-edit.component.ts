74691579137ad92abcdbc2f84004392b580181fd
Amir Sejdinovic <amir24V@yahoo.com>
2020-11-16 11:34:41 +0100

    Passing the ingredients to edit mode

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import {RecipeService} from '../recipe.service';
 
@@ -36,19 +36,33 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredinets = new FormArray([]);
 
     if(this.editMode){
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath; 
      recipeDescription = recipe.description;
+     if(recipe['ingredients']){
+       for(let ingredient of recipe.ingredients){
+          recipeIngredinets.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+
+              
+            })
+          );
+       }
+     }
     }
 
 
        this.recipeForm = new FormGroup({
            'name': new FormControl(recipeName),
            'imagePath': new FormControl(recipeImagePath),
-           'description': new FormControl(recipeDescription)
+           'description': new FormControl(recipeDescription),
+           'ingredinets': recipeIngredinets
        });
   }
   
